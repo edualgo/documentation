e17125808ba6bbd52cc77c327b4761be9a666204
AmitManna99 <manna.amit99@gmail.com>
2020-12-12 20:14:12 +0530

    side nav added

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,12 @@ $(document).ready(function() {
         e.preventDefault();
 		$('body').scrollTo(target, 800, {offset: 0, 'axis':'y'});
 		
-	});
+    });
+    
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
      
     /* Bootstrap lightbox */
     /* Ref: http://ashleydw.github.io/lightbox/ */
@@ -25,7 +30,40 @@ $(document).ready(function() {
     $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
         e.preventDefault();
         $(this).ekkoLightbox();
-    });    
+    });
 
+    /*var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}*/
+
+    $('pre a.copy-btn').click(function(e) {
+
+        var text = $(this).parent().text().trim();
+        var copyVal = document.createElement('input');
+        copyVal.value = text
+        document.body.appendChild(copyVal);
+        copyVal.select();
+        document.execCommand('copy');
+        console.log(copyVal.value)
+        document.body.removeChild(copyVal);
+      });
 
 });
+
+/*
+<div class="doc-body row">
+          <div class="doc-content col-md-9 col-12 order-1">
+
+
+*/ 
