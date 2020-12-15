ad194bffe1990dd79cc18abdf2b03cba647e9fe5
Abhijit Tripathy <52751963+Abhijit2505@users.noreply.github.com>
2020-12-15 21:52:18 +0530

    Revert "User-Flow Updated Partially (#5)" (#6)
    This reverts commit d0ff2129a7d0d44b63334c782d3fe0d232d16d7a.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,31 @@
-$(document).ready(function () {
-
-  /* ===== Stickyfill ===== */
-  /* Ref: https://github.com/wilddeer/stickyfill */
-  // Add browser support to position: sticky
-  var elements = $('.sticky');
-  Stickyfill.add(elements);
-
-
-  /* Activate scrollspy menu */
-  $('body').scrollspy({ target: '#doc-menu', offset: 100 });
-
-  /* Smooth scrolling */
-  $('a.scrollto').on('click', function (e) {
-    //store hash
-    var target = this.hash;
-    e.preventDefault();
-    $('body').scrollTo(target, 800, { offset: 0, 'axis': 'y' });
-
-  });
-
-  $('#navbarCollapse').on('click', function () {
-    $('#navbar').toggleClass('active');
-  });
+$(document).ready(function() {
+	
+    /* ===== Stickyfill ===== */
+    /* Ref: https://github.com/wilddeer/stickyfill */
+    // Add browser support to position: sticky
+    var elements = $('.sticky');
+    Stickyfill.add(elements);
 
 
-  /* Bootstrap lightbox */
-  /* Ref: http://ashleydw.github.io/lightbox/ */
+    /* Activate scrollspy menu */
+    $('body').scrollspy({target: '#doc-menu', offset: 100});
+    
+    /* Smooth scrolling */
+	$('a.scrollto').on('click', function(e){
+        //store hash
+        var target = this.hash;    
+        e.preventDefault();
+		$('body').scrollTo(target, 800, {offset: 0, 'axis':'y'});
+		
+	});
+     
+    /* Bootstrap lightbox */
+    /* Ref: http://ashleydw.github.io/lightbox/ */
 
-  $(document).delegate('*[data-toggle="lightbox"]', 'click', function (e) {
-    e.preventDefault();
-    $(this).ekkoLightbox();
-  });
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
+        e.preventDefault();
+        $(this).ekkoLightbox();
+    });    
 
-  /*$.getJSON('../../navbar.html', function (data) {
-
-    console.log(data);
-    document.getElementById('navbar').innerHTML(data);
-      
-  });*/
-
-
-  // ==== Adding Copy Button
-  $('#copy-btn').click(function (e) {
-
-    var text = $(this).parent().text().trim();
-    var copyVal = document.createElement('input');
-    copyVal.value = text
-    document.body.appendChild(copyVal);
-    copyVal.select();
-    document.execCommand('copy');
-    document.body.removeChild(copyVal);
-
-    var tooltip = document.getElementById("tooltip");
-    tooltip.innerHTML = "Copied Text";
-  });
-
-  $('#copy-btn').mouseout(function (e) {
-    var tooltip = document.getElementById("tooltip");
-    tooltip.innerHTML = "Copy to clipboard";
-  });
 
 });
-
-fetch('../../navbar.html')
-  .then(response => response.text())
-  .then(text => {
-    let nabvar = document.getElementById('navbar');
-
-    nabvar.innerHTML = text;
-  })
